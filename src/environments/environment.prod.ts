9a059fdfe5e0e2048a306423edead67111c5f471
Gabriele Picco <piccogabriele@gmail.com>
2019-03-07 19:55:26 +0100

    Added firebase production environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDzaOPlkLkzs-Zxy1OrW1zEXqX1ueDbUBU',
+    authDomain: 'hackernews-e0c75.firebaseapp.com',
+    databaseURL: 'https://hackernews-e0c75.firebaseio.com',
+    projectId: 'hackernews-e0c75',
+    storageBucket: 'hackernews-e0c75.appspot.com',
+    messagingSenderId: '914185917144'
+  }
 };
