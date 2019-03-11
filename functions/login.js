3b428b69a9820f2840e14791c493caa147b55d34
Gabriele Picco <piccogabriele@gmail.com>
2019-03-11 17:50:47 +0100

    Try netlify lambda

--- functions/login.js
+++ functions/login.js
@@ -1,21 +1,6 @@
-const axios = require('axios')
-
-exports.handler = function instagram(event, context, callback) {
-  const endpoint = 'https://news.ycombinator.com/login?goto=news'
-
-  axios
-    .get(endpoint)
-    .then(({ data: { data: result } }) => {
-    callback(null, {
+exports.handler = function(event, context, callback) {
+  callback(null, {
     statusCode: 200,
-      headers: {
-      'content-type': 'text/html',
-    },
-    body: result,
-  ),
-  })
-})
-.catch((e) => {
-    callback(e)
-  })
-}
+    body: "Hello, World"
+  });
+};
