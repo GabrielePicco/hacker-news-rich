6986c1f0b31c48459382dca86f34c216c7053f03
Gabriele Picco <piccogabriele@gmail.com>
2019-03-11 18:01:35 +0100

    Lambda

--- functions/login.js
+++ functions/login.js
@@ -1,6 +1,24 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
+const axios = require('axios');
+
+const LOGIN_ENDPOINT = "https://news.ycombinator.com/login";
+
+exports.handler = async (event, context) => {
+  const endpoint = 'https://api.instagram.com/v1/users/self/media/recent'
+  const token = process.env.INSTAGRAM_ACCESS_TOKEN
+  const limit = 5
+
+  axios
+    .get('LOGIN_ENDPOINT')
+    .then(({ data: { data: res } }) => {
+    callback(null, {
     statusCode: 200,
-    body: "Hello, World"
-  });
+      headers: {
+      'content-type': 'text/html',
+    },
+    body: res,
+  })
+})
+.catch((e) => {
+    callback(e);
+  })
 };
